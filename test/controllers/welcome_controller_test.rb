e2cad5e3f8457d1e0be6a6e6f24b68e5ab6380d4
monpeco <monpeco@gmail.com>
2018-08-02 21:22:22 +0000

    Add test case for title in index, sample and direction page

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,11 +4,19 @@ class WelcomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select "title", "index"
   end
 
   test "should get sample" do
     get :sample
     assert_response :success
+    assert_select "title", "sample"
   end
   
+  test "should get direction page" do
+    get :direction
+    assert_response :success
+    assert_select "title", "directions"
+  end
+
 end
